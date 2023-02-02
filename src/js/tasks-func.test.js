fa0195550387628de8091de47e09eba9a0e84ea2
Naji-97 <elbairabennaji@gmail.com>
2023-02-02 13:35:01 +0100

    test the add function

---
+++ src/js/tasks-func.test.js
@@ -0,0 +1,38 @@
+/**
+ * @jest-environment jsdom
+ */
+
+// import the functions that you want to test
+import Todolist from './tasks-function.js';
+
+// mock the localStorage objecty
+document.body.innerHTML = '<ul class=\'task-list\'></ul>';
+
+describe('Add task', () => {
+ test('array not to be null', () => {
+   const task = new Todolist();
+   task.addtask('Microverse', false, 0);
+   expect(task.length).not.toBeNull();
+ });
+ test('Add task, description', () => {
+   const task = new Todolist();
+   task.addtask('Microverse', false, 0);
+   task.addtask('Microverse1', false, 1);
+   expect(task.taskData[1].description).toEqual('Microverse1');
+ });
+ test('Add task, status', () => {
+   const task = new Todolist();
+   task.addtask('Microverse', false, 0);
+   task.addtask('Microverse1', true, 1);
+   expect(task.taskData[1].completed).toEqual(true);
+ });
+ test('Add task, index', () => {
+   const task = new Todolist();
+   task.addtask('Microverse', false, 0);
+   task.addtask('Microverse1', false, 1);
+   task.addtask('Microverse3', false, 2);
+   expect(task.taskData[1].index).toEqual(1);
+ });
+});
+
+
--- src/js/tasks-function.js
+++ src/js/tasks-function.js
@@ -121,7 +121,7 @@ export default class Todolist {
     });
     if (typeof window !== 'undefined') {
       localStorage.setItem('TodoListDB', JSON.stringify(this.taskData));
-      window.location.reload();
+      window.location.reload(true);
     }
   }
 }
